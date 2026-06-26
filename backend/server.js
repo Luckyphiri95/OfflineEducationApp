e520352dd492cd6780c31b85dba37e0bd1943fa9
RalstonJnr <ralstonjnr@gmail.com>
2026-06-26 13:17:16 +0200

    Week 2: Subject CRUD API completed (GET, POST, PUT, DELETE)

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const db = require("./database/database");
 const authRoutes = require("./routes/authRoutes");
+const subjectRoutes = require("./routes/subjectRoutes");
 
 const app = express();
 
@@ -13,6 +14,10 @@ app.use(express.json());
 
 // AUTH ROUTES
 app.use("/api/auth", authRoutes);
+
+// SUBJECT ROUTES
+app.use("/api/subjects", subjectRoutes);
+console.log("Subject routes loaded");
 
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
